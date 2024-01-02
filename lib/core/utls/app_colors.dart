b54d0caaa7ade75e167abc0fd4f3bcf8699bd42b
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 07:16:41 +0200

    add floating action button

--- lib/core/utls/app_colors.dart
+++ lib/core/utls/app_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const kGreyColor = Color(0xffDDE6ED);
+  static const knavyColor = Color(0xff8696FE);
 }
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do/features/home/presentation/widget/custom_divider.dart';
+import 'package:to_do/features/home/presentation/widget/custom_floating_action_button.dart';
 import 'package:to_do/features/home/presentation/widget/head_text_widget.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,6 +9,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      floatingActionButton: CustomFloatingActionButton(),
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
