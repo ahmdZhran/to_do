a959d875258bc3901fca73aeb9c8f64e77818e30
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 16:06:41 +0200

    create add task view

---
+++ lib/features/add_task/presentation/views/add_task_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTaskView extends StatelessWidget {
+  const AddTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
