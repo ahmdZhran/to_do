c8f2698241cfe7271666893ecef6f22c35c2bea5
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 05:37:11 +0200

    add app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:to_do/core/utls/app_strings.dart';
 import 'package:to_do/features/home/presentation/views/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ToDo());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ToDo extends StatelessWidget {
+  const ToDo({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'To Do',
+      title: AppStrings.appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
