1ca607a83292301b2874ffb87245e3cd8429ef31
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 06:37:32 +0200

    create text styles

---
+++ lib/core/utls/text_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomTextStyle {
+  static const soraBoldstyleBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+}
