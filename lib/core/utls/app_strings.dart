9d0ac0543b8dfef9aba2847d46aed0f3b1bc9217
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 18:05:23 +0200

    add strings for add task view

--- lib/core/utls/app_strings.dart
+++ lib/core/utls/app_strings.dart
@@ -1,6 +1,14 @@
 abstract class AppStrings {
   static const String appName = 'E-commerce',
-      // Strings to Home View
+      // Strings fo Home View
       myTasks = 'My Tasks',
-      planYourDay = 'Plan your day, pave your way.';
+      planYourDay = 'Plan your day, pave your way.',
+      //String for Add task view
+      addNewTask = 'Add New Task',
+      whatAreYouPlaying = 'What are you planing',
+      addNote = 'Add Note',
+      time = 'Time',
+      date = 'Date',
+      addTask = 'Add Task',
+      deleteTask = 'Delete Task';
 }
