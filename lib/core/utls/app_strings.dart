85e16e6733e1d3d5a1f89181f67d1e33ee233742
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 05:29:08 +0200

    first initial

---
+++ lib/core/utls/app_strings.dart
@@ -0,0 +1,6 @@
+abstract class AppStrings {
+  static const String appName = 'E-commerce',
+      // Strings to Home View
+      myTasks = 'My Tasks',
+      letsMakeItProductive = 'Lets Make it Productive';
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/features/home/presentation/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'To Do',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
