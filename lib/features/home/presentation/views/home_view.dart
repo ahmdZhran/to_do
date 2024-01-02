30420806c6ef48a6180a94be1ecf2bdd4ec6601f
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 15:49:42 +0200

    convert to statefull widget

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -7,25 +7,30 @@ import 'package:to_do/features/home/presentation/widget/head_text_widget.dart';
 import 'package:to_do/features/home/presentation/widget/task_indicator_row.dart';
 
 // Defining the HomeView class, extending StatelessWidget
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   // Constructor for the HomeView class
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
     // Scaffold widget for the overall screen structure
-    return Scaffold(
+    return const Scaffold(
       // Floating action button for adding new tasks
-      floatingActionButton: const CustomFloatingActionButton(),
+      floatingActionButton: CustomFloatingActionButton(),
       // SafeArea to ensure content is not obscured by device insets
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
             // Empty space at the top of the screen
-            const SliverToBoxAdapter(child: SizedBox(height: 20)),
+            SliverToBoxAdapter(child: SizedBox(height: 20)),
 
             // Row for displaying a heading text in the center
-            const SliverToBoxAdapter(
+            SliverToBoxAdapter(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -35,15 +40,15 @@ class HomeView extends StatelessWidget {
             ),
 
             // Custom divider for separating sections
-            const CustomDivider(),
+            CustomDivider(),
 
             // Row for displaying task completion indicators
-            const SliverToBoxAdapter(
+            SliverToBoxAdapter(
               child: TaskIndicatorRow(),
             ),
 
             // Empty space between sections
-            const SliverToBoxAdapter(child: SizedBox(height: 20)),
+            SliverToBoxAdapter(child: SizedBox(height: 20)),
 
             // Custom card for displaying a list of tasks
             SliverToBoxAdapter(
