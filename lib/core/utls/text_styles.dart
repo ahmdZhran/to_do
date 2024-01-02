cd3898d0c46a22b9c934e0629c13285b4b72304c
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-02 06:49:19 +0200

    add font family name to textStyle

--- lib/core/utls/text_styles.dart
+++ lib/core/utls/text_styles.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 abstract class CustomTextStyle {
   static const normalBoldstyleBold = TextStyle(
-    fontSize: 30,
+    fontSize: 35,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Kanit',
   );
 }
