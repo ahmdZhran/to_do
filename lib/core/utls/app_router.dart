924d45f7026cbcd4c012486c5a0b8936bfd9a102
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-03 10:00:26 +0200

    add BlocProvider on AddTaskView

--- lib/core/utls/app_router.dart
+++ lib/core/utls/app_router.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do/features/add_task/presentation/view_model/cubit/add_task_cubit/add_task_cubit.dart';
 import 'package:to_do/features/add_task/presentation/views/add_task_view.dart';
 import 'package:to_do/features/home/presentation/views/home_view.dart';
 
@@ -8,7 +10,11 @@ class AppRouter {
       case '/':
         return MaterialPageRoute(builder: (_) => const HomeView());
       case '/addTaskView':
-        return MaterialPageRoute(builder: (_) => const AddTaskView());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => AddTaskCubit(),
+                  child: const AddTaskView(),
+                ));
       default:
         return MaterialPageRoute(builder: (_) => const HomeView());
     }
