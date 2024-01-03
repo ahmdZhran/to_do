aa4586b037b05e80d4f6134c782c052e57800fcc
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-03 10:08:12 +0200

    add comments on code & refactor it

--- lib/features/add_task/presentation/views/add_task_view.dart
+++ lib/features/add_task/presentation/views/add_task_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:to_do/core/utls/app_colors.dart';
-import 'package:to_do/core/utls/app_strings.dart';
 import 'package:to_do/features/add_task/presentation/widgets/back_to_home_view.dart';
-import 'package:to_do/features/add_task/presentation/widgets/custom_button.dart';
 import 'package:to_do/features/add_task/presentation/widgets/custom_form_widget.dart';
+import 'package:to_do/features/add_task/presentation/widgets/get_buttons.dart';
 import 'package:to_do/features/add_task/presentation/widgets/head_add_task_text.dart';
 import 'package:to_do/features/add_task/presentation/widgets/sub_text.dart';
 
@@ -12,45 +10,25 @@ class AddTaskView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height: 40),
-            const BackToHomeView(),
-            const SizedBox(height: 40),
-            const HeadAddTaskText(),
-            const SizedBox(height: 100),
-            const SubTextHead(),
-            const CustomFormWidget(),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 23),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: 150,
-                    height: 50,
-                    child: CustomButton(
-                      color: AppColors.kGreyColor,
-                      onPressed: () {},
-                      text: const Text(AppStrings.deleteTask),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 150,
-                    height: 50,
-                    child: CustomButton(
-                      onPressed: () {},
-                      color: AppColors.knavyColor,
-                      text: const Text(AppStrings.addTask),
-                    ),
-                  ),
-                ],
-              ),
-            )
+            SizedBox(height: 40),
+            // Widget to navigate back to the home view
+            BackToHomeView(),
+            SizedBox(height: 40),
+            // Widget for the heading of the add task view
+            HeadAddTaskText(),
+            SizedBox(height: 100),
+            // Widget for the subheading of the add task view
+            SubTextHead(),
+            // Widget for the custom form to input task details
+            CustomFormWidget(),
+            SizedBox(height: 20),
+            // Widget to get buttons for actions in the add task view
+            GetButtons()
           ],
         ),
       ),
