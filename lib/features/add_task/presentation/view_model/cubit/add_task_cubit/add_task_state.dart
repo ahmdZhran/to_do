82c61f4c93ab1adb60debfa388da2f27e3f38c32
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-03 08:12:11 +0200

    create add task cubit and add its states

---
+++ lib/features/add_task/presentation/view_model/cubit/add_task_cubit/add_task_state.dart
@@ -0,0 +1,16 @@
+part of 'add_task_cubit.dart';
+
+@immutable
+abstract class AddTaskState {}
+
+class AddTaskInitial extends AddTaskState {}
+
+class AddTaskSuccess extends AddTaskState {}
+
+class AddTaskLoading extends AddTaskState {}
+
+class AddTaskFailer extends AddTaskState {
+  final String errMessage;
+
+  AddTaskFailer(this.errMessage);
+}
